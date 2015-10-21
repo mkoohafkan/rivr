8e57d674ba215919faef24dfbbde17c6cec72d96
mkoohafkan <michael.koohafkan@gmail.com>
2015-10-21 16:18:46 -0700

    moved shiny to inst/shiny

--- inst/shiny/server.r
+++ inst/shiny/server.r
@@ -33,24 +33,15 @@ shinyServer(function(input, output) {
   }, include.colnames = FALSE)
   output$example_table = renderTable({setNames(
     data.frame(
-      c("M1", "M2", "S1", "S2"),
-      c("2.7", "0.64", "2.0", "1.0"),
+      c("M1", "M2", "S2", "S3"),
+      c("2.7", "0.64", "2.65", "0.5"),
       c("250", "250", "250", "250"),
-      c("0.001", "0.001", "0.1", "0.1"),
+      c("0.001", "0.001", "0.005", "0.005"),
       c("100", "100", "10", "10"),
-      c("0.045", "0.045", "0.04", "0.04"),
+      c("0.045", "0.045", "0.01", "0.01"),
       rep("US Customary", 4)
     ),
     c("Profile type", "Control section depth", "Channel flow", 
     "Channel slope", "channel width", "Manning's roughness", "Units"))
   }, include.rownames = FALSE)
 })
-#' # example M1 profile
-#' compute_profile(0.001, 0.045, 250, 2.7, 1.486, 32.2, 100, 0, stepdist=50, totaldist=3000)
-#' # example M2 profile
-#' compute_profile(0.001, 0.045, 250, 0.64, 1.486, 32.2, 100, 0, stepdist=50, totaldist=3000)
-#' # example S2 profile
-#' compute_profile(0.1, 0.04, 250, 2.0, 1.486, 32.2, 10, 0, stepdist=0.1, totaldist=3000)
-#' # example S3 profile
-#' compute_profile(0.1, 0.04, 250, 1.0, 1.486, 32.2, 10, 0, stepdist=0.1, totaldist=3000)
-
