061a16550718da4f4ca8684470cecbe46a752d92
mkoohafkan <michael.koohafkan@gmail.com>
2015-10-26 08:20:59 -0700

    better inclusion of shiny apps via function `demo_shiny`

---
+++ R/shinydemos.r
@@ -0,0 +1,31 @@
+#' Shiny Demonstrations
+#'
+#' Demonstrate package functionality via Shiny apps
+#'
+#' @param ex Example to run.
+#'
+#' @details Demonstrations available:
+#'   \code{"gvf"} Gradually-varied flow.
+#'
+#' @examples
+#' # get list of available demos
+#' demo_shiny()
+#' # run the gradually-varied flow demo
+#' demo_shiny("gvf")
+#' @export
+demo_shiny <- function(ex) {
+  # locate all the shiny app examples that exist
+  app.dir <- system.file("shiny-examples", package = "rivr")
+  valid.examples <- list.files(app.dir)
+  if(missing(ex))
+    return(
+      paste0("Available shiny examples: '", 
+             paste(valid.examples, collapse = "', '"), "'")
+    )
+  if(!(ex %in% valid.examples))
+    stop("example '", ex, "' not found")  
+  if(!require(shiny))
+    stop("package 'shiny' is not installed.")
+  shiny::runApp(file.path(app.dir, ex), display.mode = "normal")
+  invisible(NULL)
+}
