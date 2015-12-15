f0cbf9370109219597402947fa990871296b9f66
mkoohafkan <michael.koohafkan@gmail.com>
2015-12-14 23:11:42 -0800

    Added shiny demo function `demo_shiny` and files to reproduce upcoming R Journal publication "Open-Channel Computation with R".

--- R/shinydemos.r
+++ R/shinydemos.r
@@ -1,22 +1,22 @@
-#' Shiny Demonstrations
-#'
-#' Demonstrate package functionality via Shiny apps
-#'
+#' @title Shiny Demonstrations
+#' @description Demonstrate package functionality via Shiny apps
 #' @param ex Example to run.
-#'
 #' @details Demonstrations available:
 #'   \code{"gvf"} Gradually-varied flow.
 #'
 #' @examples
+#' \dontrun{
 #' # get list of available demos
 #' demo_shiny()
 #' # run the gradually-varied flow demo
 #' demo_shiny("gvf")
+#' }
+#'
 #' @export
-demo_shiny <- function(ex) {
+demo_shiny = function(ex){
   # locate all the shiny app examples that exist
-  app.dir <- system.file("shiny-examples", package = "rivr")
-  valid.examples <- list.files(app.dir)
+  app.dir = system.file("shiny-examples", package = "rivr")
+  valid.examples = list.files(app.dir)
   if(missing(ex))
     return(
       paste0("Available shiny examples: '", 
@@ -24,7 +24,7 @@ demo_shiny <- function(ex) {
     )
   if(!(ex %in% valid.examples))
     stop("example '", ex, "' not found")  
-  if(!require(shiny))
+  if(!requireNamespace("shiny", quietly = TRUE))
     stop("package 'shiny' is not installed.")
   shiny::runApp(file.path(app.dir, ex), display.mode = "normal")
   invisible(NULL)
